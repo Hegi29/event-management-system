674361813c2c510c29ae03d7c06f8973e5362794
Hegi29 <hegitrisaputra@gmail.com>
2024-07-28 12:22:06 +0700

    feat: add image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,15 +1,25 @@
-const WaitingReviewImage = require('./WaitingReview.png');
-const UnderReviewImage = require('./UnderReview.png');
+const DetailEventImage = require('./DetailEvent.png');
+const EventRequestingRevisionImage = require('./EventRequestingRevision.png');
+const ExportImage = require('./Export.png');
+const LogomarkImage = require('./Logomark.png');
+const MenuImage = require('./Menu.png');
+const NotificationImage = require('./Notification.png');
 const RequestRevisionImage = require('./RequestRevision.png');
 const ReviewCompleteImage = require('./ReviewComplete.png');
-const ExportImage = require('./Export.png');
-const DetailEventImage = require('./DetailEvent.png');
+const TopVenueImage = require('./TopVenue.png');
+const UnderReviewImage = require('./UnderReview.png');
+const WaitingReviewImage = require('./WaitingReview.png');
 
 export {
     DetailEventImage,
     ExportImage,
+    LogomarkImage,
+    MenuImage,
+    NotificationImage,
+    EventRequestingRevisionImage,
     RequestRevisionImage,
     ReviewCompleteImage,
+    TopVenueImage,
     UnderReviewImage,
     WaitingReviewImage
 }
