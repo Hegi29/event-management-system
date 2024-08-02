5117ed95daf7d60cf49350b8e3d9b1a85ee09227
Hegi29 <hegitrisaputra@gmail.com>
2024-08-02 16:15:35 +0700

    chore: artifacts release apk

---
+++ android/app/src/main/assets/images/index.ts
@@ -0,0 +1,55 @@
+const DetailEventImage = require('./DetailEvent.png');
+const EventRequestingRevisionImage = require('./EventRequestingRevision.png');
+const ExportImage = require('./Export.png');
+const LogomarkImage = require('./Logomark.png');
+const LogOutImage = require('./LogOut.png');
+const MenuImage = require('./Menu.png');
+const NotificationImage = require('./Notification.png');
+const RequestRevisionImage = require('./RequestRevision.png');
+const ReviewCompleteImage = require('./ReviewComplete.png');
+const TopVenueImage = require('./TopVenue.png');
+const UnderReviewImage = require('./UnderReview.png');
+const WaitingReviewImage = require('./WaitingReview.png');
+
+const PhoneImage = require('./Phone.png');
+const CalendarImage = require('./Calendar.png');
+const CheckedImage = require('./Checked.png');
+
+const EmptyFileImage = require('./EmptyFile.png');
+const ExcelFileImage = require('./ExcelFile.png');
+const ImageFileImage = require('./ImageFile.png');
+const WarningFileImage = require('./WarningFile.png');
+const QuestionFileImage = require('./QuestionFile.png');
+const UploadImage = require('./Upload.png');
+const SelectVenueImage = require('./SelectVenue.png');
+const RingImage = require('./Ring.png');
+const DeleteImage = require('./Delete.png');
+const ChangeImage = require('./Change.png');
+
+export {
+    CalendarImage,
+    ChangeImage,
+    CheckedImage,
+    DeleteImage,
+    DetailEventImage,
+    EmptyFileImage,
+    ExcelFileImage,
+    ExportImage,
+    EventRequestingRevisionImage,
+    ImageFileImage,
+    LogomarkImage,
+    LogOutImage,
+    MenuImage,
+    NotificationImage,
+    PhoneImage,
+    RequestRevisionImage,
+    ReviewCompleteImage,
+    RingImage,
+    SelectVenueImage,
+    TopVenueImage,
+    UnderReviewImage,
+    UploadImage,
+    WaitingReviewImage,
+    WarningFileImage,
+    QuestionFileImage
+}
